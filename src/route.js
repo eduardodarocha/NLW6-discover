15d02773e50f7429e77aed77bd2de5e89ad5de7d
Eduardo Rocha <eduardomfrocha@gmail.com>
2021-06-26 11:05:30 -0300

    Aula 5

--- src/route.js
+++ src/route.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const QuestionController = require('./controllers/QuestionController');
-const RoomController = require('./controllers/RoomController');
+const express = require('express')
+const QuestionController = require('./controllers/QuestionController')
+const RoomController = require('./controllers/RoomController')
 
 const route = express.Router()
 
@@ -8,15 +8,13 @@ const route = express.Router()
 route.get('/', (req, res) => res.render("index", {page: 'enter-room'}))
 route.get('/create-pass', (req,res) => res.render("index", {page: 'create-pass'}))
 
-route.get('/room', (req, res) => res.render("room"));
-route.get('/create-pass', (req,res) => res.render("create-pass"));
-
-route.get('/room/:room', (req, res) => res.render("room"))
-
- // Formato que o formulario de dentro da modal tem que passar a informação
- // route.post('/room/:room/:question/:action'), (req,res) => res.render("exemplo", { req })
- 
-route.post('/question/:room/:question/:action', QuestionController.index)
 route.post('/create-room', RoomController.create)
+route.get('/room/:room', RoomController.open)
+route.post('/enterroom', RoomController.enter)
+
+// Formato que o formulario de dentro da modal tem que passar a informação
+ // route.post('/room/:room/:question/:action'), (req,res) => res.render("exemplo", { req })
+route.post('/question/create/:room', QuestionController.create)
+route.post('/question/:room/:question/:action', QuestionController.index)
 
 module.exports = route
